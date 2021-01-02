41d2fa9d0837241949dc965ec8b04efa56587061
bagglerock <bagglerock@gmail.com>
2021-01-02 02:03:40 -0500

    feat: update package.json to compile backend each time it changes

--- frontend/src/App/Layout/Main/Main.tsx
+++ frontend/src/App/Layout/Main/Main.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
-export const Main: React.FC = () => (
-  <main className="main p-3">
-    <p>Hangman, coming soon..</p>
-  </main>
-);
+export const Main: React.FC = () => {
+  useEffect(() => {
+    axios.get('/api/test').then(response => {
+      console.log('test', response);
+    });
+  }, []);
+
+  return (
+    <main className="main p-3">
+      <p>Hangman, coming soon..</p>
+    </main>
+  );
+};
