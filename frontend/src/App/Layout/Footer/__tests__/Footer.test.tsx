3e31e7fac15f61f115ff4d2bd1ef48f850634b3b
bagglerock <bagglerock@gmail.com>
2021-01-03 01:54:01 -0500

    test: formatting

--- frontend/src/App/Layout/Footer/__tests__/Footer.test.tsx
+++ frontend/src/App/Layout/Footer/__tests__/Footer.test.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 test('renders App/Layout', () => {
   Date.now = jest.fn(() => new Date('2000-01-01T01:00:00Z').getTime());
+
   const { container } = render(<Footer />);
 
   expect(container).toMatchSnapshot();
