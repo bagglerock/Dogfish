c12fbcf9cd384175824ef06514d3e87d694fa6ee
bagglerock <bagglerock@gmail.com>
2021-01-22 00:20:01 -0500

    refactor: clean up RunningView... extract hook

--- frontend/src/App/HangmanGame/Views/RunningView/RunningView.tsx
+++ frontend/src/App/HangmanGame/Views/RunningView/RunningView.tsx
@@ -1,38 +1,17 @@
 import { IGameState } from 'app/HangmanGame/HangmanGameContainer';
 import { useWord } from 'app/HangmanGame/hooks/useWord';
 import { GameStats } from 'app/HangmanGame/models/GameStats';
-import { GAME_STATE, HIDDEN_CHARACTER } from 'app/HangmanGame/share/const';
 import { GameStatistics } from 'app/HangmanGame/Views/RunningView/Sections/GameStatistics/GameStatistics';
 import { LettersGuessed } from 'app/HangmanGame/Views/RunningView/Sections/LettersGuessed/LettersGuessed';
 import { StatusMessages } from 'app/HangmanGame/Views/RunningView/Sections/StatusMessages/StatusMessages';
 import { WordDisplay } from 'app/HangmanGame/Views/RunningView/Sections/WordDisplay/WordDisplay';
-import { indexOf } from 'lodash';
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useUpdateStats } from './useUpdateStats';
 
 export const RunningView: React.FC<IGameState & RunningViewProps> = ({ setGameState, word, stats, setStats }) => {
   const { maskedWord, lettersGuessed, remainingGuesses } = useWord(word);
 
-  useEffect(() => {
-    if (indexOf(maskedWord, HIDDEN_CHARACTER) === -1) {
-      setStats(stats => {
-        return new GameStats({ ...stats, wins: stats.wins + 1 });
-      });
-
-      setGameState(GAME_STATE.WIN);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [maskedWord]);
-
-  useEffect(() => {
-    if (remainingGuesses === 0) {
-      setStats(stats => {
-        return new GameStats({ ...stats, losses: stats.losses + 1 });
-      });
-
-      setGameState(GAME_STATE.LOSE);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [remainingGuesses]);
+  useUpdateStats(setGameState, setStats, maskedWord, remainingGuesses);
 
   const statusMessage = 'This is a test message';
 
---
+++ frontend/src/App/HangmanGame/Views/RunningView/useUpdateStats.tsx
@@ -0,0 +1,33 @@
+import { GameStats } from 'app/HangmanGame/models/GameStats';
+import { GAME_STATE, HIDDEN_CHARACTER } from 'app/HangmanGame/share/const';
+import { indexOf } from 'lodash';
+import React, { useEffect } from 'react';
+
+export const useUpdateStats = (
+  setGameState: React.Dispatch<React.SetStateAction<GAME_STATE>>,
+  setStats: React.Dispatch<React.SetStateAction<GameStats>>,
+  maskedWord: string[],
+  remainingGuesses: number
+) => {
+  useEffect(() => {
+    if (indexOf(maskedWord, HIDDEN_CHARACTER) === -1) {
+      setStats(stats => {
+        return new GameStats({ ...stats, wins: stats.wins + 1 });
+      });
+
+      setGameState(GAME_STATE.WIN);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [maskedWord]);
+
+  useEffect(() => {
+    if (remainingGuesses === 0) {
+      setStats(stats => {
+        return new GameStats({ ...stats, losses: stats.losses + 1 });
+      });
+
+      setGameState(GAME_STATE.LOSE);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [remainingGuesses]);
+};
